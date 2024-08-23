71e6cf17b3fcff6d626fe7b7610732728c5e95cc
OrlinAguilar <oraguilarv@unah.hn>
2024-08-23 08:19:08 -0600

    reviciony correccion de  los repositorios 100% y listo

--- src/main/java/hn/unah/demo/repositorios/TBL_SUELDOSRepository.java
+++ src/main/java/hn/unah/demo/repositorios/TBL_SUELDOSRepository.java
@@ -3,9 +3,9 @@ package hn.unah.demo.repositorios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import hn.unah.demo.modelos.TBL_HISTORIAL_PAGOS;
+import hn.unah.demo.modelos.TBL_SUELDOS;
 
 @Repository
-public interface TBL_HISTORIAL_PAGOSRepository extends JpaRepository<TBL_HISTORIAL_PAGOS, Long> {
+public interface TBL_SUELDOSRepository extends JpaRepository<TBL_SUELDOS, Long> {
 
 }
